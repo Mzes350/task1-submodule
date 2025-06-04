9f8cfbd79a7fb6d3a0ff1bf8119c5f7b468e1673
Mzes350 <goliathxemo@gmail.com>
2025-06-04 16:54:05 +0300

    task 1 commit 2

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
-#stdio.h
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
-{
+ {
+    int Age = 20;
+    float Height = 5.9;
+    double Distance = 12345.6789;
+    char Initial = 'A';
+
+    printf( "Age %d\n", Age);
+    printf("Height %f\n", Height);
+    printf("Distance %lf\n", Distance);
+    printf("Initial %c\n",Initial);
 
 
-    return 0
-}
+
+
+    return 0;}
